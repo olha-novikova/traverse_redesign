0608eee2caa92b21a4520b27aeb1eda88a18d9c9
Sergiu026 <synthezys@gmail.com>
2017-10-25 11:46:17 +0300

    added my pitches and my opportunities pages

---
+++ themes/workscout-child/recent-opportunities.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Page Recent Opportunities
+ *
+ */
+
+
+if (!session_id())
+	session_start();
+
+$user = wp_get_current_user();
+$currency = get_woocommerce_currency_symbol();
+if ( in_array( 'candidate', (array) $user->roles ) || in_array( 'administrator', (array) $user->roles ) ) :
+	$applications_list = get_candidate_account_balance_info($user->ID);
+	$available_cash = get_candidate_cash_out_sum($user->ID);
+
+	else: wp_redirect(home_url());
+endif;
+
+get_header('new');
+global $wpdb;
+get_sidebar();?>
+	<main class="main">
+<?php get_template_part('template-parts/page-header')?>
+	<div class="content">
+		<?php get_template_part('template-parts/recent-opportunities')?>
+	</div>
+<?php
+get_footer('new');
+?>
---
+++ themes/workscout-child/template-parts/my-pitches.php
@@ -0,0 +1,162 @@
+<div class="section__container section__container_pitches">
+	<div class="table table_pitches">
+		<div class="table__head">
+			<div class="table__row table__row_header">
+				<div class="table__header">
+					<p class="table__text">pitched campaigns</p>
+				</div>
+			</div>
+		</div>
+		<div class="table__body">
+			<?php
+			$pitches = get_pitched_campaigns($user->ID, 'new');
+
+			if ($pitches) : ?>
+				<?php foreach ($pitches as $pitch) : ?>
+					<?php $pitches_data = get_post_meta($pitch[0]->ID, '', true);
+					$date = $pitch['date'];
+					?>
+					<div class="table__row table__row_body">
+						<div class="table__data"><i class="icon icon_calendar"></i>
+							<p class="table__data__date"><?php echo date("d", strtotime($date)) ?></p>
+							<p class="table__data__month"><?php echo date("F", strtotime($date)) ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text">
+								<?php
+								echo esc_html( $pitches_data['_job_title'][0]); echo ($pitches_data['_company_name'][0] != '' ? 'for <span>' . esc_html($pitches_data['_company_name'][0]) : '');
+								?>
+								</span>
+							</p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo ($pitches_data['_job_location'][0] ? esc_html($pitches_data['_job_location'][0]) : 'Anywhere') ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo esc_html($pitch['message'])?></p>
+						</div>
+						<div class="table__data">
+							<div class="table__buttons">
+								<div class="table__buttons">
+									<div class="button button_green"><a href="<?= get_post_permalink($pitch[0]->ID) ?>">View Campaign Details</a></div>
+									<div class="button button_green"><a href="<?= get_post_permalink($pitch['id']) ?>">View Full Pitch</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endforeach; else: ?>
+				<div class="table__body">
+					<div class="table__row table__row_body table__row_empty">
+						<div class="empty"><i class="icon icon_calendar"></i>
+							<p class="empty-text">There are no completed campaigns to show</p>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+		<div class="table__head">
+			<div class="table__row table__row_header">
+				<div class="table__header">
+					<p class="table__text">completed campaigns</p>
+				</div>
+			</div>
+		</div>
+		<div class="table__body">
+			<?php
+			$pitches = get_pitched_campaigns($user->ID, 'completed');
+
+			if ($pitches) : ?>
+				<?php foreach ($pitches as $pitch) : ?>
+					<?php $pitches_data = get_post_meta($pitch[0]->ID, '', true);
+					$date = $pitch['date'];
+
+					?>
+					<div class="table__row table__row_body">
+						<div class="table__data"><i class="icon icon_calendar"></i>
+							<p class="table__data__date"><?php echo date("d", strtotime($date)) ?></p>
+							<p class="table__data__month"><?php echo date("F", strtotime($date)) ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text">
+								<?php
+								echo esc_html( $pitches_data['_job_title'][0]); echo ($pitches_data['_company_name'][0] != '' ? 'for <span>' . esc_html($pitches_data['_company_name'][0]) : '');
+								?>
+							</p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo ($pitches_data['_job_location'][0] ? esc_html($pitches_data['_job_location'][0]) : 'Anywhere') ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo esc_html($pitch['message'])?></p>
+						</div>
+						<div class="table__data">
+							<div class="table__buttons">
+								<div class="button button_green"><a href="<?= get_post_permalink($pitch[0]->ID) ?>">View Campaign Details</a></div>
+								<div class="button button_green"><a href="<?= get_post_permalink($pitch['id']) ?>">View Full Pitch</a></div>
+							</div>
+						</div>
+					</div>
+				<?php endforeach; else: ?>
+				<div class="table__body">
+					<div class="table__row table__row_body table__row_empty">
+						<div class="empty"><i class="icon icon_calendar"></i>
+							<p class="empty-text">There are no completed campaigns to show</p>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+		<div class="table__head">
+			<div class="table__row table__row_header">
+				<div class="table__header">
+					<p class="table__text">Current  Campaign</p>
+				</div>
+			</div>
+		</div>
+		<div class="table__body">
+			<?php
+			$pitches = get_pitched_campaigns($user->ID, 'hired');
+
+			if ($pitches) : ?>
+				<?php foreach ($pitches as $pitch) : ?>
+					<?php $pitches_data = get_post_meta($pitch[0]->ID, '', true);
+					$date = $pitch['date'];
+					?>
+					<div class="table__row table__row_body">
+						<div class="table__data"><i class="icon icon_calendar"></i>
+							<p class="table__data__date"><?php echo date("d", strtotime($date)) ?></p>
+							<p class="table__data__month"><?php echo date("F", strtotime($date)) ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text">
+								<?php
+								echo esc_html( $pitches_data['_job_title'][0]); echo ($pitches_data['_company_name'][0] != '' ? ' for <span>' . esc_html($pitches_data['_company_name'][0]) : '');
+								?>
+								</span>
+							</p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo ($pitches_data['_job_location'][0] ? esc_html($pitches_data['_job_location'][0]) : 'Anywhere') ?></p>
+						</div>
+						<div class="table__data">
+							<p class="table__text"><?php echo esc_html($pitch['message'])?></p>
+						</div>
+						<div class="table__data">
+							<div class="table__buttons">
+								<div class="button button_green"><a href="<?= get_post_permalink($pitch[0]->ID) ?>">View Campaign Details</a></div>
+								<div class="button button_green"><a href="<?= get_post_permalink($pitch['id']) ?>">View Full Pitch</a></div>
+							</div>
+						</div>
+					</div>
+				<?php endforeach; else: ?>
+				<div class="table__body">
+					<div class="table__row table__row_body table__row_empty">
+						<div class="empty"><i class="icon icon_calendar"></i>
+							<p class="empty-text">There are no completed campaigns to show</p>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
