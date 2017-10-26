af7af24e2f49ebd7ac4fa2ce7d23606299d1343c
Sergiu026 <synthezys@gmail.com>
2017-10-26 17:35:17 +0300

    css fixes

--- themes/workscout-child/sidebar.php
+++ themes/workscout-child/sidebar.php
@@ -2,6 +2,9 @@
 /**/
 ?>
 <sidebar class="sidebar">
+  <div class="logo">
+    <p class="logo__text">T</p>
+  </div>
     <ul class="icon-list">
         <li class="icon-list__element"><i class="icon icon_menu"></i></li>
         <li class="icon-list__element"><i class="icon icon_newsfeed"></i></li>
